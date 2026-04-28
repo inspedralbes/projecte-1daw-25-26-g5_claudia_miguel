618622e4ac03bdafb78046777988a3e1f17b5efd
a23migfulbel <a23migfulbel@inspedralbes.cat>
2026-04-28 13:20:42 +0200

    He cambiado los textos y añadido el footer

--- web/index.php
+++ web/index.php
@@ -11,14 +11,10 @@
 <?php include 'encabezado_titulo.php'; ?>
 
 <div class="px-4 py-5 my-5 text-center">
- <img class="d-block mx-auto mb-4" src="/fotos/logo.jpeg" alt="" width="72" height="57">
- <h1 class="display-5 fw-bold text-body-emphasis">Centered hero</h1>
+ <img src="fotos/logo.jpeg" alt="" width="72" height="72">
+ <h1 class="display-5 fw-bold text-body-emphasis">Selecciona qui ets</h1>
  <div class="col-lg-6 mx-auto">
-     <p class="lead mb-4">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p> 
- <div class="d-grid gap-2 d-sm-flex justify-content-sm-center"> 
-    <button type="button" class="btn btn-primary btn-lg px-4 gap-3">Primary button</button> 
-    <button type="button" class="btn btn-outline-secondary btn-lg px-4">Secondary</button> 
- </div> 
+     <p class="lead mb-4">Una vegada seleccionis qui ets t'enredigirem a un altre pagina on podras seleccionar el que pots fer</p> 
 </div> 
 </div>
 
@@ -53,7 +49,7 @@
     </div>
   </div>
 </div>
-
+<?php include 'pie.php'; ?>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
